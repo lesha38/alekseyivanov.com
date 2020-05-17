b4b09c795fd9493ad10cb78eaadfd0bb6e5f609c
Aleksey Ivanov <ivanov@trueconf.ru>
2020-05-17 23:11:24 +0300

    init project

---
+++ src/screens/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="developing">
+    <p class="developing-desc">
+      COMING SOON
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    data: () => ({
+
+    }),
+  };
+</script>
+
+<style lang="scss">
+  .developing {
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(black, 0.6);
+    border-radius: 2px;
+    box-sizing: border-box;
+    padding: 24px;
+  }
+
+  .developing-desc {
+    color: white;
+    font-weight: 500;
+    font-size: 32px;
+  }
+</style>
---
+++ src/screens/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main-screen">
+    <div class="main-screen__main">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MainScreen',
+    data: () => ({
+
+    }),
+  };
+</script>
+
+<style lang="scss">
+
+</style>
